1358f4c2aba1caef32d984a89ae92585a1bb9b96
gavin boone <gavin-boone@ejgallo.com>
2026-07-24 20:13:32 +0000

    added <leader>gc for quick commits

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -6,5 +6,11 @@ return {
     { "<leader>gd", "<cmd>Gdiffsplit<CR>", desc = "Git diff split" },
     { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git blame" },
     { "<leader>gl", "<cmd>Git log --oneline<CR>", desc = "Git log" },
+    { "<leader>gc", function() 
+      local msg = vim.fn.input("Commit message: ")
+      if msg ~= "" then
+        vim.cmd(":!git commit -a -m " .. vim.fn.shellescape(msg))
+      end
+    end, desc = "Git commit" },
   },
 }
